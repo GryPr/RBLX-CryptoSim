819288070651eed01817ea36effdde19e5319102
Kevin <promkev@gmail.com>
2020-12-30 21:48:28 -0500

    Start points of interests

---
+++ src/server/poi.server.ts
@@ -0,0 +1,28 @@
+import { CollectionService } from "@rbxts/services"
+const Players = game.GetService("Players");
+
+// Detects players who touch the selling point
+for (const obj of CollectionService.GetTagged("SellPoint")) {
+
+    if (obj.IsA("Part")){
+
+        obj.Touched.Connect((part) => {
+            if (obj.Parent?.IsA("Model") && obj.Parent?.FindFirstAncestorOfClass("Humanoid")){
+                if (Players.GetPlayerFromCharacter(part.Parent) !== undefined){
+                    //let player:Player = Players.GetPlayerFromCharacter(part.Parent);
+
+                }
+            }
+        })
+
+        obj.TouchEnded.Connect((part) => {
+            if (obj.Parent?.IsA("Model") && obj.Parent?.FindFirstAncestorOfClass("Humanoid")){
+                if (Players.GetPlayerFromCharacter(part.Parent) !== undefined){
+                    //let player:Player = Players?.GetPlayerFromCharacter(part.Parent);
+
+                }
+            }
+        })
+
+    }
+}
