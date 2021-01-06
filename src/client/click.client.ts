b72b8734e6105e369626ecbabdf195b2abe601af
Kevin <promkev@gmail.com>
2021-01-05 23:26:34 -0500

    Improve clicking behavior

--- src/client/click.client.ts
+++ src/client/click.client.ts
@@ -7,24 +7,30 @@ const localMouse = localPlayer.GetMouse();
 // Detects clicks
 localMouse.Button1Down.Connect(() => Click());
 
+let running: boolean = false;
+
 // TODO: Check that it doesnt' get stuck on mobile
 function Click() {
-  let running: boolean = true;
-  spawn(() => {
-    while (running) {
-      client.playerClick();
-      wait(0.2);
-      let stillRunning: boolean = false;
-      for (const element of UserInputService.GetMouseButtonsPressed()) {
-        if (element.UserInputType.Name === "MouseButton1") {
-          stillRunning = true;
+  if (running === true) {
+    running = false;
+  } else {
+    running = true;
+    spawn(() => {
+      while (running) {
+        client.playerClick();
+        wait(0.2);
+        let stillRunning: boolean = false;
+        for (const element of UserInputService.GetMouseButtonsPressed()) {
+          if (element.UserInputType.Name === "MouseButton1") {
+            stillRunning = true;
+          }
+        }
+        if (stillRunning === false) {
+          running = false;
         }
       }
-      if (stillRunning === false) {
-        running = false;
-      }
-    }
-  });
+    });
+  }
 }
 
 spawn(() => {
